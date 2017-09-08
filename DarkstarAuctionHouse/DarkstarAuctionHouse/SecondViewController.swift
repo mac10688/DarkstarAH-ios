37e8547f71692c1e1fd00e21016c42c756f44f51
Matthew Cooper <mcooper@servpronet.com>
2017-09-07 18:57:28 -0700

    Creating initial project

---
+++ DarkstarAuctionHouse/DarkstarAuctionHouse/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  DarkstarAuctionHouse
+//
+//  Created by Matthew Cooper on 9/7/17.
+//  Copyright © 2017 Darkstar. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
